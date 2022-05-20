468158446e7e6ec55580491a99e30d21d863b7b5
Abhishek Bisht <abhi.bisht89@gmail.com>
2022-05-20 17:31:09 +0530

    Time: 85 ms, Memory: 17.4 MB - LeetHub

--- linked-list-cycle/linked-list-cycle.py
+++ linked-list-cycle/linked-list-cycle.py
@@ -14,7 +14,7 @@ class Solution:
         fast=head
         
         while fast and fast.next:
-            fast=fast.next and fast.next.next
+            fast=fast.next.next
             slow=slow.next
             if fast==slow:
                 return True
