becce13469b0f4dc1815a2ebea0b975afff69da5
Abhishek Bisht <abhi.bisht89@gmail.com>
2022-06-01 20:32:43 +0530

    Time: 71 ms (24.80%), Space: 14.1 MB (73.38%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -4,10 +4,15 @@ class Solution:
         if len(nums)==1:
             return nums[0]
         
-        arr=[0]*len(nums)
-        arr[0]=nums[0]
+#         arr=[0]*len(nums)
+#         arr[0]=nums[0]
+        
+#         for i in range(1,len(nums)):
+#             arr[i]=arr[i-1]+nums[i]
+            
+#         return arr  
         
         for i in range(1,len(nums)):
-            arr[i]=arr[i-1]+nums[i]
+            nums[i]=nums[i-1]+nums[i]
             
-        return arr    
+        return nums    
