03ecfd1639e865c3df24d86b2493757cbf2b9178
Abhishek Bisht <abhi.bisht89@gmail.com>
2022-06-11 20:38:26 +0530

    Time: 36 ms (79.02%), Space: 13.9 MB (49.99%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
@@ -1,9 +1,22 @@
 class Solution:
     def removePalindromeSub(self, s: str) -> int:
         #If the s is not pallindrome, it will always be reduced to empty string in 2 steps else it can         be reduce to empty string in 1 step.
-        rev_str=s[::-1]
         
-        if rev_str==s:
-            return 1
-        else:
-            return 2
+        
+#         rev_str=s[::-1]
+        
+#         if rev_str==s:
+#             return 1
+#         else:
+#             return 2
+        
+        #complexity
+        #Time - O(n)
+        #Space - O(n) - space for reverse string
+        
+        if len(s)==0:
+            return 0
+        for i in range(len(s)//2):
+            if s[i]!=s[len(s)-i-1]:
+                return 2
+        return 1    
